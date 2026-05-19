9e7d630a813830bb51dafbc9d15a265b644db438
Anis <anis.trabelsi1@gmail.com>
2026-05-19 14:14:20 +0100

    feat(studio): add French localization plugin for Sanity Studio interface

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { media } from 'sanity-plugin-media'
+import { frFRLocale } from '@sanity/locale-fr-fr'
 import { schemaTypes } from './src/sanity/schemas'
 import { structure } from './src/sanity/structure'
 
@@ -12,7 +13,7 @@ export default defineConfig({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
 
-  plugins: [structureTool({ structure }), visionTool(), media()],
+  plugins: [structureTool({ structure }), visionTool(), media(), frFRLocale()],
 
   schema: {
     types: schemaTypes,
